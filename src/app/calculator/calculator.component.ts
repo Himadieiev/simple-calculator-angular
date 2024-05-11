c9149f561f9ad7b149d5c1141a5e75f34229affc
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-05-11 23:39:01 +0300

    added calculator-comp

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.scss',
+})
+export class CalculatorComponent {
+  public firstNumber = new FormControl(null);
+  public secondNumber = new FormControl(null);
+  public result: number | undefined = undefined; //public result?: number;
+  public operation = new FormControl('+');
+
+  public calculation() {
+    switch (this.operation.value) {
+      case '+':
+        this.result = Number(this.firstNumber.value) + Number(this.secondNumber.value);
+        break;
+      case '-':
+        this.result = Number(this.firstNumber.value) - Number(this.secondNumber.value);
+        break;
+      case '*':
+        this.result = Number(this.firstNumber.value) * Number(this.secondNumber.value);
+        break;
+      case '/':
+        this.result = Number(this.firstNumber.value) / Number(this.secondNumber.value);
+        break;
+    }
+  }
+}
